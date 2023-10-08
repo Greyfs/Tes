c25ae54fa54ed382316d327bc1167cce75deeb1d
Grey <125029422+Greyfs@users.noreply.github.com>
2023-10-08 20:17:54 +0700

    Add files via upload
    Kompenen di tambahkan

---
+++ components/footer.js
@@ -0,0 +1,178 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import Container from "./container";
+
+export default function Footer() {
+  const navigation = ["Product", "Features", "Pricing", "Company", "Blog"];
+  const legal = ["Terms", "Privacy", "Legal"];
+
+  return (
+    <div className="relative">
+      <Container>
+        <div className="grid max-w-screen-xl grid-cols-1 gap-10 pt-10 mx-auto mt-5 border-t border-gray-100 dark:border-trueGray-700 lg:grid-cols-5">
+          <div className="lg:col-span-2">
+            <div>
+              {" "}
+              <Link
+                href="/"
+                className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100"
+              >
+                <Image
+                  src="/img/logo.svg"
+                  alt="N"
+                  width="32"
+                  height="32"
+                  className="w-8"
+                />
+                <span>Starlynime</span>
+              </Link>
+            </div>
+
+            <div className="max-w-md mt-4 text-gray-500 dark:text-gray-400">
+              Nextly is a free landing page & marketing website template for startups and indie projects. It's built with Next.js & TailwindCSS and it's completely open-source.
+            </div>
+
+            <div className="mt-5">
+              <a
+                href="https://vercel.com/?utm_source=web3templates&utm_campaign=oss"
+                target="_blank"
+                rel="noopener"
+                className="relative block w-44"
+              >
+                <Image
+                  src="/img/vercel.svg"
+                  alt="Powered by Vercel"
+                  width="212"
+                  height="44"
+                />
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
+              {navigation.map((item, index) => (
+                <Link
+                  key={index}
+                  href="/"
+                  className="w-full px-4 py-2 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700"
+                >
+                  {item}
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div>
+            <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
+              {legal.map((item, index) => (
+                <Link
+                  key={index}
+                  href="/"
+                  className="w-full px-4 py-2 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700"
+                >
+                  {item}
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div className="">
+            <div>Follow us</div>
+            <div className="flex mt-5 space-x-5 text-gray-400 dark:text-gray-500">
+              <a
+                href="https://facebook.com/web3templates"
+                target="_blank"
+                rel="noopener"
+              >
+                <span className="sr-only">Facebook</span>
+                <Facebook />
+              </a>
+              <a
+                href="https://instagram.com/web3templates"
+                target="_blank"
+                rel="noopener"
+              >
+                <span className="sr-only">Instagram</span>
+                <Instagram />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
+          Copyright © {new Date().getFullYear()}. All part of the ALTI organization by{" "}
+          <a
+            href="https://github.com/greyfs"
+            target="_blank"
+            rel="noopener"
+          >
+            Greyfs.
+          </a>{" "}
+          All Right Reserved{" "}
+          <a
+            href="https://github.com/greyfs"
+            target="_blank"
+            rel="noopener"
+          >
+            Starlynime.web.id
+          </a>
+        </div>
+      </Container>
+      {/* Do not remove this */}
+      <Backlink />
+    </div>
+  );
+}
+
+const Facebook = ({ size = 24 }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
+    <path
+      d="M24 12.07C24 5.41 18.63 0 12 0S0 5.4 0 12.07C0 18.1 4.39 23.1 10.13 24v-8.44H7.08v-3.49h3.04V9.41c0-3.02 1.8-4.7 4.54-4.7 1.31 0 2.68.24 2.68.24v2.97h-1.5c-1.5 0-1.96.93-1.96 1.89v2.26h3.32l-.53 3.5h-2.8V24C19.62 23.1 24 18.1 24 12.07"
+    />
+  </svg>
+);
+
+const Instagram = ({ size = 24 }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
+    <path
+      d="M16.98 0a6.9 6.9 0 0 1 5.08 1.98A6.94 6.94 0 0 1 24 7.02v9.96c0 2.08-.68 3.87-1.98 5.13A7.14 7.14 0 0 1 16.94 24H7.06a7.06 7.06 0 0 1-5.03-1.89A6.96 6.96 0 0 1 0 16.94V7.02C0 2.8 2.8 0 7.02 0h9.96zm.05 2.23H7.06c-1.45 0-2.7.43-3.53 1.25a4.82 4.82 0 0 0-1.3 3.54v9.92c0 1.5.43 2.7 1.3 3.58a5 5 0 0 0 3.53 1.25h9.88a5 5 0 0 0 3.53-1.25 4.73 4.73 0 0 0 1.4-3.54V7.02a5 5 0 0 0-1.3-3.49 4.82 4.82 0 0 0-3.54-1.3zM12 5.76c3.39 0 6.2 2.8 6.2 6.2a6.2 6.2 0 0 1-12.4 0 6.2 6.2 0 0 1 6.2-6.2zm0 2.22a3.99 3.99 0 0 0-3.97 3.97A3.99 3.99 0 0 0 12 15.92a3.99 3.99 0 0 0 3.97-3.97A3.99 3.99 0 0 0 12 7.98zm6.44-3.77a1.4 1.4 0 1 1 0 2.8 1.4 1.4 0 0 1 0-2.8z" />
+  </svg>
+);
+
+const Backlink = () => {
+  return (
+    <a
+      href="https://github.com/greyfs" // Ganti USERNAME dengan nama pengguna GitHub Anda
+      target="_blank"
+      rel="noopener"
+      className="absolute flex px-3 py-1 space-x-2 text-sm font-semibold text-gray-900 bg-white border border-gray-300 rounded shadow-sm place-items-center left-5 bottom-5 dark:bg-trueGray-900 dark:border-trueGray-700 dark:text-trueGray-300"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        fill="currentColor"
+        className="w-4 h-4"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fillRule="evenodd"
+          d="M8 .198a8.002 8.002 0 0 0-2.533 15.602c.4.074.547-.174.547-.385 0-.19-.007-.693-.010-1.36-2.002.37-2.923-.49-3.113-.94-.105-.27-.556-.94-.948-1.13-.813-.295-2.053.245-1.625.24.395-.005.677.37.775.52.452.77 1.47.54 1.84.41.057-.33.28-.54.51-.66-1.785-.203-3.644-.893-3.644-3.974 0-.88.31-1.6.82-2.17-.08-.202-.36-1.03.08-2.14 0 0 .67-.22 2.2.83.64-.18 1.32-.27 2-.27.68 0 1.36.09 2-.27 1.52-1.05 2.20-.83 2.20-.83.44 1.11.16 1.94.08 2.14.51.57.82 1.29.82 2.17 0 3.09-1.86 3.77-3.65 3.97.28.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.20 0 .21.14.46.55.38A8.002 8.002 0 0 0 8 .198z"
+        />
+      </svg>
+      <span>Founder</span>
+    </a>
+  );
+};
