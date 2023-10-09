ba22391e79f611020cea95b517be8d75243fed79
Grey <125029422+Greyfs@users.noreply.github.com>
2023-10-09 18:18:05 +0700

    Create forum.js

---
+++ pages/forum.js
@@ -0,0 +1,9 @@
+// pages/forum.js
+
+import Forum from '../components/forum'; // Sesuaikan dengan struktur direktori Anda
+
+const ForumPage = () => {
+  return <Forum />;
+};
+
+export default ForumPage;
