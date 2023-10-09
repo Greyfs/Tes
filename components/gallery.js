8fc3750f859a05c0d676f8d98c6f51400cacf5bf
Grey <125029422+Greyfs@users.noreply.github.com>
2023-10-09 18:04:29 +0700

    Update gallery.js

--- components/gallery.js
+++ components/gallery.js
@@ -1,34 +1,7 @@
-import Link from 'next/link';
-import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Gallery = () => {
-  useEffect(() => {
-    const pesen = "HAHA NICE TRY DUDE 😂, enak aja😎👍";
-
-    function clickIE4() {
-      if (event.button === 2) {
-        alert(pesen);
-        return false;
-      }
-    }
-
-    function clickNS4(e) {
-      if (document.layers || (document.getElementById && !document.all)) {
-        if (e.which === 2 || e.which === 3) {
-          alert(pesen);
-          return false;
-        }
-      }
-    }
-
-    if (document.layers) {
-      document.captureEvents(Event.MOUSEDOWN);
-      document.onmousedown = clickNS4;
-    } else if (document.all && !document.getElementById) {
-      document.onmousedown = clickIE4;
-    }
-    document.oncontextmenu = new Function("alert(pesen);return false");
-  }, []);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   const images = [
     { url: 'https://telegra.ph/file/1d5ae9d104855f2ee5e2c.jpg', description: 'Deskripsi Gambar 1', downloadLink: 'https://example.com/download-link-1' },
@@ -36,22 +9,44 @@ const Gallery = () => {
     // Tambahkan gambar dan deskripsi lainnya di sini
   ];
 
+  const handleImageClick = (index) => {
+    alert('Klik tombol "Download" untuk mengunduh gambar.');
+  };
+
+  const handleTextCopy = () => {
+    alert('Salinan teks dilarang.');
+  };
+
   return (
     <div className="container mx-auto mt-5 p-5">
       <h1 className="text-2xl font-bold mb-3">Gallery</h1>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         {images.map((image, index) => (
-          <div key={index} className="border p-3 rounded-lg">
-            <img src={image.url} alt={`Image ${index}`} className="w-full h-auto" />
+          <div
+            key={index}
+            className={`border p-3 rounded-lg transition-transform transform ${
+              hoveredIndex === index ? 'hover:scale-105 opacity-75' : 'opacity-100'
+            }`}
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
+            onClick={() => handleImageClick(index)}
+          >
+            <div className="relative">
+              <img src={image.url} alt={`Image ${index}`} className="w-full h-auto" />
+              <div
+                className="absolute bottom-0 left-0 right-0 bg-black text-white text-center p-2 opacity-0 transition-opacity hover:opacity-90"
+              >
+                {image.description}
+              </div>
+            </div>
             <div className="mt-2">
-              <p>{image.description}</p>
-              <Link href={image.downloadLink}>
-                <a
-                  className="text-white bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded-full cursor-pointer text-center block"
-                >
-                  Download
-                </a>
-              </Link>
+              <p onCopy={handleTextCopy}>{image.description}</p>
+              <a
+                href={image.downloadLink}
+                className="text-white bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded-full cursor-pointer text-center block"
+              >
+                Download
+              </a>
             </div>
           </div>
         ))}
