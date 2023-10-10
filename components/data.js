64c94e9589bb65288f5c0164f7c0db1cf6763073
Grey <125029422+Greyfs@users.noreply.github.com>
2023-10-10 19:55:57 +0700

    Update data.js

--- components/data.js
+++ components/data.js
@@ -11,50 +11,49 @@ import benefitOneImg from "../public/img/benefit-one.png";
 import benefitTwoImg from "../public/img/benefit-two.png";
 
 const benefitOne = {
-  title: "Highlight your benefits",
-  desc: "You can use this space to highlight your first benefit or a feature of your product. It can also contain an image or Illustration like in the example along with some bullet points.",
+  title: "Highlightkan Keuntungan Anda",
+  desc: "Anda dapat menggunakan ruang ini untuk menyoroti keuntungan pertama atau fitur produk Anda. Ini juga dapat berisi gambar atau ilustrasi seperti contoh di atas bersama dengan beberapa poin poin penting.",
   image: benefitOneImg,
   bullets: [
     {
-      title: "Understand your customers",
-      desc: "Then explain the first point breifly in one or two lines.",
+      title: "Mengerti Pelanggan Anda",
+      desc: "Kemudian jelaskan poin pertama secara singkat dalam satu atau dua kalimat.",
       icon: <FaceSmileIcon />,
     },
     {
-      title: "Improve acquisition",
-      desc: "Here you can add the next benefit point.",
+      title: "Meningkatkan Akuisisi",
+      desc: "Di sini Anda dapat menambahkan poin keuntungan berikutnya.",
       icon: <ChartBarSquareIcon />,
     },
     {
-      title: "Drive customer retention",
-      desc: "This will be your last bullet point in this section.",
+      title: "Mendorong Retensi Pelanggan",
+      desc: "Ini akan menjadi poin terakhir Anda dalam bagian ini.",
       icon: <CursorArrowRaysIcon />,
     },
   ],
 };
 
 const benefitTwo = {
-  title: "Offer more benefits here",
-  desc: "You can use this same layout with a flip image to highlight your rest of the benefits of your product. It can also contain an image or Illustration as above section along with some bullet points.",
+  title: "Tawarkan Lebih Banyak Keuntungan di Sini",
+  desc: "Anda dapat menggunakan tata letak yang sama dengan gambar berbalik untuk menyoroti sisa keuntungan produk Anda. Ini juga dapat berisi gambar atau ilustrasi seperti bagian di atas bersama dengan beberapa poin penting.",
   image: benefitTwoImg,
   bullets: [
     {
-      title: "Mobile Responsive Template",
-      desc: "Nextly is designed as a mobile first responsive template.",
+      title: "Template Responsif untuk Ponsel",
+      desc: "Nextly dirancang sebagai template responsif ponsel pertama.",
       icon: <DevicePhoneMobileIcon />,
     },
     {
-      title: "Powered by Next.js & TailwindCSS",
-      desc: "This template is powered by latest technologies and tools.",
+      title: "Ditenagai oleh Next.js & TailwindCSS",
+      desc: "Template ini didukung oleh teknologi dan alat terbaru.",
       icon: <AdjustmentsHorizontalIcon />,
     },
     {
-      title: "Dark & Light Mode",
-      desc: "Nextly comes with a zero-config light & dark mode. ",
+      title: "Mode Gelap & Terang",
+      desc: "Nextly dilengkapi dengan mode gelap & terang tanpa konfigurasi. ",
       icon: <SunIcon />,
     },
   ],
 };
 
-
-export {benefitOne, benefitTwo};
+export { benefitOne, benefitTwo };
