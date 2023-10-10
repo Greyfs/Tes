045b6564fa2dd4a527b72f2c6e995452a56d66aa
Grey <125029422+Greyfs@users.noreply.github.com>
2023-10-10 21:27:33 +0700

    Update testimonials.js

--- components/testimonials.js
+++ components/testimonials.js
@@ -6,49 +6,48 @@ import userOneImg from "../public/img/user1.jpg";
 import userTwoImg from "../public/img/user2.jpg";
 import userThreeImg from "../public/img/user3.jpg";
 
-const Testimonials  = () => {
+const Ulasan  = () => {
   return (
     <Container>
       <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
         <div className="lg:col-span-2 xl:col-auto">
           <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
             <p className="text-2xl leading-normal ">
-              Share a real <Mark>testimonial</Mark>
-              that hits some of your benefits from one of your popular customer.
+              Bagikan <Mark>ulasan</Mark> nyata
+              yang mencakup beberapa keuntungan dari salah satu pelanggan populer Anda.
             </p>
 
             <Avatar
               image={userOneImg}
               name="Sarah Steiner"
-              title="VP Sales at Google"
+              title="VP Sales di Google"
             />
           </div>
         </div>
         <div className="">
           <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
             <p className="text-2xl leading-normal ">
-              Make sure you only pick the <Mark>right sentence</Mark>
-              to keep it short and simple.
+              Pastikan Anda hanya memilih <Mark>kalimat yang tepat</Mark>
+              untuk menjaga agar pendek dan sederhana.
             </p>
 
             <Avatar
               image={userTwoImg}
               name="Dylan Ambrose"
-              title="Lead marketer at Netflix"
+              title="Pemasar Utama di Netflix"
             />
           </div>
         </div>
         <div className="">
           <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
             <p className="text-2xl leading-normal ">
-              This is an <Mark>awesome</Mark> landing page template I&apos;ve seen. I
-              would use this for anything.
+              Ini adalah template halaman landing yang <Mark>keren</Mark> yang pernah saya lihat. Saya akan menggunakannya untuk segala hal.
             </p>
 
             <Avatar
               image={userThreeImg}
               name="Gabrielle Winn"
-              title="Co-founder of Acme Inc"
+              title="Co-founder dari Acme Inc"
             />
           </div>
         </div>
@@ -88,4 +87,4 @@ function Mark(props) {
   );
 }
 
-export default Testimonials;
+export default Ulasan;
