d4a8bb21f108c7ea7fbeba02cfdd232a216b1deb
Grey <125029422+Greyfs@users.noreply.github.com>
2023-10-11 08:16:39 +0700

    Update testimonials.js

--- components/testimonials.js
+++ components/testimonials.js
@@ -1,5 +1,5 @@
+import Image from "next/image";
 import React from "react";
-import { Image } from "next/image";
 import Container from "./container";
 
 import userOneImg from "../public/img/user1.jpg";
@@ -7,52 +7,48 @@ import userTwoImg from "../public/img/user2.jpg";
 import userThreeImg from "../public/img/user3.jpg";
 
 const Testimonials = () => {
+  const testimonialsData = [
+    {
+      name: "3 orang team",
+      title: "Kerja sama",
+      description: "Untuk membuat konten berkualitas, kami memiliki tim untuk terus memberikan informasi dan update seputar anime.",
+      image: userOneImg,
+    },
+    {
+      name: "Berita panas",
+      title: "Topic yang sedang viral",
+      description: "Kami sebagai Blog website yang memberikan berita seputar anime akan menampilkan banyak informasi yang sedang trending dan panas untuk dibicarakan.",
+      image: userTwoImg,
+    },
+    {
+      name: "Pembahasan yang terpilih",
+      title: "Artikel yang berkualitas",
+      description: "Selain banyaknya hal di atas, kami memperhatikan kualitas artikel kami, agar relevan untuk banyak orang.",
+      image: userThreeImg,
+    },
+  ];
+
   return (
     <Container>
       <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
-        <div className="lg:col-span-2 xl:col-auto">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="text-2xl leading-normal">
-              Untuk membuat konten <Mark>berkualitas</Mark>, kami memiliki tim untuk terus memberikan informasi dan update seputar anime.
-            </p>
-
-            <Avatar
-              image={userOneImg}
-              name="3 orang team"
-              title="Kerja sama"
-            />
+        {testimonialsData.map((testimonial, index) => (
+          <div key={index} className="lg:col-span-2 xl:col-auto">
+            <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
+              <p className="text-2xl leading-normal">
+                {testimonial.description}
+              </p>
+              <Avatar
+                image={testimonial.image}
+                name={testimonial.name}
+                title={testimonial.title}
+              />
+            </div>
           </div>
-        </div>
-        <div className="">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="text-2xl leading-normal">
-              Kami sebagai Blog website yang memberikan <Mark>berita</Mark> seputar anime akan menampilkan banyak informasi yang sedang trending dan panas untuk dibicarakan.
-            </p>
-
-            <Avatar
-              image={userTwoImg}
-              name="Berita panas"
-              title="Topic yang sedang viral"
-            />
-          </div>
-        </div>
-        <div className="">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="text-2xl leading-normal">
-              Selain banyaknya hal di atas, kami memperhatikan kualitas artikel kami, agar relevan untuk banyak <Mark>orang</Mark>.
-            </p>
-
-            <Avatar
-              image={userThreeImg}
-              name="Pembahasan yang terpilih"
-              title="Artikel yang berkualitas"
-            />
-          </div>
-        </div>
+        ))}
       </div>
     </Container>
   );
-};
+}
 
 function Avatar(props) {
   return (
@@ -60,8 +56,8 @@ function Avatar(props) {
       <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
         <Image
           src={props.image}
-          width={40}
-          height={40}
+          width="40"
+          height="40"
           alt="Avatar"
           placeholder="blur"
         />
@@ -71,17 +67,6 @@ function Avatar(props) {
         <div className="text-gray-600 dark:text-gray-400">{props.title}</div>
       </div>
     </div>
-  );
-}
-
-function Mark(props) {
-  return (
-    <>
-      {" "}
-      <mark className="text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">
-        {props.children}
-      </mark>{" "}
-    </>
   );
 }
 
