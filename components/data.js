b17eef008b164bdb73a1ab1ecf2a77f737c200cf
Grey <125029422+Greyfs@users.noreply.github.com>
2023-10-11 07:25:10 +0700

    Update data.js

--- components/data.js
+++ components/data.js
@@ -11,46 +11,46 @@ import benefitOneImg from "../public/img/benefit-one.png";
 import benefitTwoImg from "../public/img/benefit-two.png";
 
 const benefitOne = {
-  title: "Highlightkan Keuntungan Anda",
-  desc: "Anda dapat menggunakan ruang ini untuk menyoroti keuntungan pertama atau fitur produk Anda. Ini juga dapat berisi gambar atau ilustrasi seperti contoh di atas bersama dengan beberapa poin poin penting.",
+  title: "Membuat hari kalian menyenangkan",
+  desc: "Diwebsite Starlynime Kalian bisa mendapatkan berbagai informasi dan hal menarik seperti gambar dengan resolusi tinggi dengan gratis untuk kalian pakai sebagai wallpaper kalian.",
   image: benefitOneImg,
   bullets: [
     {
-      title: "Mengerti Pelanggan Anda",
-      desc: "Kemudian jelaskan poin pertama secara singkat dalam satu atau dua kalimat.",
+      title: "Website yang menyenangkan",
+      desc: "Website dan blog yang mudah di jelajahi dengan tidak banyaknya iklan",
       icon: <FaceSmileIcon />,
     },
     {
-      title: "Meningkatkan Akuisisi",
-      desc: "Di sini Anda dapat menambahkan poin keuntungan berikutnya.",
+      title: "Konten Terupdate",
+      desc: "Kita memberikan konten terupdate dan terpopuler.",
       icon: <ChartBarSquareIcon />,
     },
     {
-      title: "Mendorong Retensi Pelanggan",
-      desc: "Ini akan menjadi poin terakhir Anda dalam bagian ini.",
+      title: "Kualitas terpilih website Starlynime",
+      desc: "Website yang Responsive dan tidak banyak bug yang menyebabkan lag",
       icon: <CursorArrowRaysIcon />,
     },
   ],
 };
 
 const benefitTwo = {
-  title: "Tawarkan Lebih Banyak Keuntungan di Sini",
-  desc: "Anda dapat menggunakan tata letak yang sama dengan gambar berbalik untuk menyoroti sisa keuntungan produk Anda. Ini juga dapat berisi gambar atau ilustrasi seperti bagian di atas bersama dengan beberapa poin penting.",
+  title: "Selain itu kami juga menyesuaikan Website dengan Gaya modern",
+  desc: "Di tahun 2023 ini orang orang lebih menyukai memakai ponsel di banding Computer, karena lebih efisien hemat dan cepat, berikut dibawah ini adalah fitur dan tampilan website yang telah disesuaikan agar cocok di perangkat mobile.",
   image: benefitTwoImg,
   bullets: [
     {
-      title: "Template Responsif untuk Ponsel",
-      desc: "Nextly dirancang sebagai template responsif ponsel pertama.",
+      title: "Responsif untuk Ponsel",
+      desc: "Starlynime dirancang sebagai Website responsif ponsel.",
       icon: <DevicePhoneMobileIcon />,
     },
     {
       title: "Ditenagai oleh Next.js & TailwindCSS",
-      desc: "Template ini didukung oleh teknologi dan alat terbaru.",
+      desc: "Website ini didukung oleh teknologi dan alat terbaru.",
       icon: <AdjustmentsHorizontalIcon />,
     },
     {
       title: "Mode Gelap & Terang",
-      desc: "Nextly dilengkapi dengan mode gelap & terang tanpa konfigurasi. ",
+      desc: "Starlynime dilengkapi dengan mode gelap & terang ini cocok untuk kalian sesuai lebih suka yang mana.",
       icon: <SunIcon />,
     },
   ],
