59c3abb2b9c4144a041d95a834d250d5b0f91538
Grey <125029422+Greyfs@users.noreply.github.com>
2023-10-11 07:42:26 +0700

    Update testimonials.js

--- components/testimonials.js
+++ components/testimonials.js
@@ -1,19 +1,19 @@
-import Image from "next/image";
 import React from "react";
+import { Image } from "next/image";
 import Container from "./container";
 
 import userOneImg from "../public/img/user1.jpg";
 import userTwoImg from "../public/img/user2.jpg";
 import userThreeImg from "../public/img/user3.jpg";
 
-const Testimonials  = () => {
+const Testimonials = () => {
   return (
     <Container>
       <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
         <div className="lg:col-span-2 xl:col-auto">
           <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="text-2xl leading-normal ">
-              Untuk membuat konten <Mark>berkualitas</Mark> kami memiliki tim untuk terus memberikan informasi dan update seputar anime
+            <p className="text-2xl leading-normal">
+              Untuk membuat konten <Mark>berkualitas</Mark>, kami memiliki tim untuk terus memberikan informasi dan update seputar anime.
             </p>
 
             <Avatar
@@ -24,9 +24,9 @@ const Testimonials  = () => {
           </div>
         </div>
         <div className="">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="text-2xl leading-normal ">
-              Kami sebagai Blog website yang memberikan </Mark>berita</Mark>seputar anime akan menampilkan banyak informasi, yang sedang trending dan panas untuk di bicarakan.
+          <div className="flex flex-col justify between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
+            <p className="text-2xl leading-normal">
+              Kami sebagai Blog website yang memberikan <Mark>berita</Mark> seputar anime akan menampilkan banyak informasi yang sedang trending dan panas untuk dibicarakan.
             </p>
 
             <Avatar
@@ -38,8 +38,8 @@ const Testimonials  = () => {
         </div>
         <div className="">
           <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="text-2xl leading-normal ">
-              Selain banyaknya hal di atas, kami memperhatikan kualitas artikel kami, agar revevan untuk banyak <Mark>Orang</Mark> 
+            <p className="text-2xl leading-normal">
+              Selain banyaknya hal di atas, kami memperhatikan kualitas artikel kami, agar relevan untuk banyak <Mark>orang</Mark>.
             </p>
 
             <Avatar
@@ -52,7 +52,7 @@ const Testimonials  = () => {
       </div>
     </Container>
   );
-}
+};
 
 function Avatar(props) {
   return (
@@ -60,8 +60,8 @@ function Avatar(props) {
       <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
         <Image
           src={props.image}
-          width="40"
-          height="40"
+          width={40}
+          height={40}
           alt="Avatar"
           placeholder="blur"
         />
@@ -76,12 +76,9 @@ function Avatar(props) {
 
 function Mark(props) {
   return (
-    <>
-      {" "}
-      <mark className="text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">
-        {props.children}
-      </mark>{" "}
-    </>
+    <mark className="text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">
+      {props.children}
+    </mark>
   );
 }
 
